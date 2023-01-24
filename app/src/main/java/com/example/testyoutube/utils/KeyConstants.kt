5034e8c67f3d681f3c87a72dc953e63422f385c5
PobWalerii <lstshef@gmail.com>
2023-01-24 13:39:57 +0200

    дизайн

--- app/src/main/java/com/example/testyoutube/utils/KeyConstants.kt
+++ app/src/main/java/com/example/testyoutube/utils/KeyConstants.kt
@@ -2,5 +2,5 @@ package com.example.testyoutube.utils
 
 object KeyConstants {
     const val BASE_URL = "https://www.googleapis.com/youtube/v3/"
-    const val API_KEY = "AIzaSyD_sRkpmmcS2ZVbx9t4Xn43VoQHCaJHkDI"
+    const val API_KEY = "YOUR_KEY"
 }
