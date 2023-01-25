3d47d3e47ee18fa0c433fd6f5c832eff3e9d68fe
Walera <lstshef@gmail.com>
2023-01-25 05:16:26 +0200

    retrofit

--- app/src/main/java/com/example/testyoutube/di/RepositoryModule.kt
+++ app/src/main/java/com/example/testyoutube/di/RepositoryModule.kt
@@ -1,5 +1,6 @@
 package com.example.testyoutube.di
 
+import com.example.testyoutube.data.api.ApiService
 import com.example.testyoutube.data.repository.VideoRepository
 import dagger.Module
 import dagger.Provides
@@ -13,8 +14,8 @@ object RepositoryModule {
 
     @Singleton
     @Provides
-    fun provideVideoRepository(): VideoRepository {
-        return VideoRepository()
+    fun provideVideoRepository(apiService: ApiService): VideoRepository {
+        return VideoRepository(apiService)
     }
 
 }
